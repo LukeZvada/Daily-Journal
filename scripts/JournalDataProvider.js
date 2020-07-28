122576400c63ccf32cb9ec235bae0dcc3be987a5
LukeZvada <zvadamusic@gmail.com>
2020-07-28 15:32:43 -0500

    API created. Entries entered.

--- scripts/JournalDataProvider.js
+++ scripts/JournalDataProvider.js
@@ -1,31 +1,19 @@
-const journal = [
-    {
-        id: 1,
-        date: "07/06/2020",
-        concept: "HTML & CSS",
-        entry: "We talked about HTML components and how to make grid layouts with Flexbox in CSS.",
-        mood: "Ok",
-    },
-    {
-        id: 2,
-        date: "07/8/2020",
-        concept: "Flexbox",
-        entry: "We learned how to take html components and style them using flexbox rules.",
-        mood: "Ok",
-    },
-    {
-        id: 3,
-        date: "07/12/2020",
-        concept: "Javascript Import/Export",
-        entry: "We learned how to import separate js files into another to accomplish a goal that is set by functions",
-        mood: "Ok",
-    },
-]
 
-export const useJournalEntries = () => {
-    const sortedByDate = journal.sort(
-        (currentEntry, nextEntry) =>
-            Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
-    )
-    return sortedByDate
+const getEntries = () => {
+    fetch('http://localhost:8088/entries')
+        .then(response => response.json())
+        .then(parsedEntries => {
+            entries = parsedEntries
+        })
+
 }
+
+
+
+// export const useJournalEntries = () => {
+//     const sortedByDate = journal.sort(
+//         (currentEntry, nextEntry) =>
+//             Date.parse(currentEntry.date) - Date.parse(nextEntry.date)
+//     )
+//     return sortedByDate
+// }
