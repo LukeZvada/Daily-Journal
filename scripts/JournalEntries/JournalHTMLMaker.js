89b93a645981db4d9ca57f60e537bad8c10823b6
LukeZvada <zvadamusic@gmail.com>
2020-08-17 11:49:34 -0700

    cleaned up json

--- scripts/JournalEntries/JournalHTMLMaker.js
+++ scripts/JournalEntries/JournalHTMLMaker.js
@@ -1,6 +1,7 @@
 export const journalEntryComponent = (entry) => {
     return `
         <section id="entry--${entry.id}" class="journalEntry">
+            Concepts Covered: ${entry.conceptsCovered}
             Entry: ${entry.entryText} 
             Date: ${entry.date}
         </section>
