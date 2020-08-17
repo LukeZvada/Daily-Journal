8528f913ebb6975c4bdc7064f989dca6de436ebf
LukeZvada <zvadamusic@gmail.com>
2020-08-17 09:44:09 -0700

    comment cleanup

--- scripts/JournalEntries/JournalEntriesList.js
+++ scripts/JournalEntries/JournalEntriesList.js
@@ -30,22 +30,3 @@ export const journalList = () => {
         })
     }
 
-    
-
-
-
-
-// export const entryListComponent = () => { 
-//     const entries = useJournalEntries()
-
-//     let entryHTMLRep = ""
-//     for (const entry of entries) { 
-//         entryHTMLRep += journalEntryComponent(entry)
-//     }
-
-//     entryLog.innerHTML += ` 
-//     <article> 
-//         ${entryHTMLRep}
-//     </article>
-//     `
-// }
