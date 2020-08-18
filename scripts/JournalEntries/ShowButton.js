f130cdabd2f01fe69646b75f19c201f382058610
LukeZvada <zvadamusic@gmail.com>
2020-08-17 17:51:32 -0700

    commenting out more console.logs

--- scripts/JournalEntries/ShowButton.js
+++ scripts/JournalEntries/ShowButton.js
@@ -2,7 +2,7 @@ const contentTarget = document.querySelector("#showEntryButton")
 const eventHub = document.querySelector("#container")
 
 eventHub.addEventListener("click", clickEvent => {
-    console.log("clicked the button")
+    // console.log("clicked the button")
     if (clickEvent.target.id === "showEntries") {
         const customEvent = new CustomEvent("showEntriesClicked")
         eventHub.dispatchEvent(customEvent)
