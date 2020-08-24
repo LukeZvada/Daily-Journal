ac54f36258d724c2a6baab1b7f46fd4bdc4267e2
LukeZvada <zvadamusic@gmail.com>
2020-08-24 08:03:21 -0700

    added useMood and getMoods

---
+++ scripts/JournalEntries/MoodDataProvider.js
@@ -0,0 +1,14 @@
+let mood = []
+
+export const useMood = () => { 
+    return mood.slice()
+}
+
+export const getMoods = () => {
+    return fetch('http://localhost:3000/entries')
+        .then(response => response.json())
+        .then(parsedMoods => {
+            entries = parsedMoods
+        })
+
+}
