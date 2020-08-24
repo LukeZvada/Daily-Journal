373761c8b3c1470a66d21805e4db505187f0848a
LukeZvada <zvadamusic@gmail.com>
2020-08-24 10:01:32 -0700

    removed console.log that was not needed

--- scripts/JournalEntries/ShowButton.js
+++ scripts/JournalEntries/ShowButton.js
@@ -2,7 +2,6 @@ const contentTarget = document.querySelector("#showEntryButton")
 const eventHub = document.querySelector("#container")
 
 eventHub.addEventListener("click", clickEvent => {
-    // console.log("clicked the button")
     if (clickEvent.target.id === "showEntries") {
         const customEvent = new CustomEvent("showEntriesClicked")
         eventHub.dispatchEvent(customEvent)
